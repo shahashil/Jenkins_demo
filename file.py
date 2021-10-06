85c9f88a63671fb36d36cbc64e3ac172894a50db
shahashil <57207261+shahashil@users.noreply.github.com>
2021-10-06 11:22:32 +0530

    Update file.py

--- file.py
+++ file.py
@@ -1,9 +1,17 @@
-print("Calculator")
-try:
-    n = int(input())
-    print(n * 10)
-    print("Addition is ",n)
-except EOFError as e:
-    print(e)
-
-
+import sys
+input_2=""
+n = input()
+m=0
+l=0
+array=[]
+main_array={}
+for i in range (0,n):
+    inp=raw_input()
+    array=(inp.split())
+    main_array[array[0]] = array[1]
+for i in range(0,n):
+    take = raw_input()
+    if take in main_array :
+        print take+"="+main_array[take]
+    else :
+        print "Not found"
