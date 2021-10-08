e634be2eaa97d1bded7087bdb90069c0d311ce77
shahashil <57207261+shahashil@users.noreply.github.com>
2021-10-08 17:47:42 +0530

    Update file.py

--- file.py
+++ file.py
@@ -1,9 +1,10 @@
+import os
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
 
-choice = input("Enter choice(1/2/3/4): ")
+choice = os.getenv("Para1")
 
 print("Your choice is ",choice)
