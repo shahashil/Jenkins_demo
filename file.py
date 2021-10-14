be0da4bcb484517dcb60ca105a2d19ecb15f45bc
shahashil <57207261+shahashil@users.noreply.github.com>
2021-10-14 19:47:38 +0530

    Update file.py

--- file.py
+++ file.py
@@ -1,11 +1,13 @@
-import os
-print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
+# import os
+# print("Select operation.")
+# print("1.Add")
+# print("2.Subtract")
+# print("3.Multiply")
+# print("4.Divide")
 
-choice = os.getenv("Para1")
+# choice = os.getenv("Para1")
 
-print("Your choice is ",choice)
-#https://stackoverflow.com/questions/47834898/pass-build-inputs-from-jenkins-to-python
+# print("Your choice is ",choice)
+# #https://stackoverflow.com/questions/47834898/pass-build-inputs-from-jenkins-to-python
+a,b = 4,7
+print("Sum of ",a," and  ",b, " is "a+b)
